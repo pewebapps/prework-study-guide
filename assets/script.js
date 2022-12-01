840300a04293af631881745a61e1fea67e5ac08a
Piers <piersebdon@gmail.com>
2022-12-01 20:37:43 +0000

    added javascrit file and linked it to index

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for (let i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'Javascript') {
+  console.log("Let's study Javascript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+
+for (let i = 0; i < shapes.length; i++) {
+  console.log(shapes[i]);
+}
